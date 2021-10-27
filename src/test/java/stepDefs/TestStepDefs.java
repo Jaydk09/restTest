b18793297c80d60dd44daab771ff732c7fea978a
jaydk09 <prn99999@gmail.com>
2021-10-27 16:07:35 +0100

    second commit

--- src/test/java/stepDefs/TestStepDefs.java
+++ src/test/java/stepDefs/TestStepDefs.java
@@ -16,15 +16,16 @@ import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestStepDefs {
-@Test
+  @Test
   @Given("user is logged")
   public void user_is_logged() {
- Response response = RestAssured.get("https://www.bbc.co.uk");
- long timeinMs = response.timeIn(TimeUnit.SECONDS);
- long ms = response.getStatusCode();
-  System.out.println("millisec = %d "+ timeinMs);
+    Response response = RestAssured.get("https://www.bbc.co.uk");
+    long timeinMs = response.timeIn(TimeUnit.SECONDS);
+    long ms = response.getStatusCode();
+    System.out.println("millisec = %d " + timeinMs);
 
   }
+
   @When("user click on picture")
   public void user_click_on_picture() {
     System.out.println("^^^^^User clicked on picture");
@@ -36,8 +37,10 @@ public class TestStepDefs {
     System.out.println("^^^^^Picture displayed");
 
   }
+
   @When("user inputs {} as {} and {} as {}")
-  public void user_inputs_firstname_as_ramesh_lastname_as_pasupuleti_account_as_sortcode_as(String firstname, String firstnamevalue,String lastname, String lastnamevalue) {
+  public void user_inputs_firstname_as_ramesh_lastname_as_pasupuleti_account_as_sortcode_as(String firstname, String firstnamevalue, String lastname, String lastnamevalue) {
+    System.out.println("user inputs......");
   }
 
 
